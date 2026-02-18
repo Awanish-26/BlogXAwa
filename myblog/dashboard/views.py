6e8566ff5946a942e63e4ac57e3097a9bddd850a
Awanish-26 <awanishyadav996@gmail.com>
2026-02-18 12:28:13 +0530

    Enhance user profile view and update templates for better post display

--- myblog/dashboard/views.py
+++ myblog/dashboard/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from blog.models import Post
 # Create your views here.
 
 
 @login_required
 def profile(request):
-    return render(request, 'dashboard/profile.html')
+    posts = Post.objects.filter(author=request.user)
+    return render(request, 'dashboard/profile.html', {'posts': posts})
